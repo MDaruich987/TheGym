ce182729d3efbf22b2ea9e710ddeefe7411c1a21
MDaruich987 <42543575+MDaruich987@users.noreply.github.com>
2018-11-03 22:05:15 -0300

    Consultar Factura de Pago gerente

---
+++ SistemasIIITHEGYM/ConsultarFacturadePagoGerente.aspx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SistemasIIITHEGYM
+{
+    public partial class ConsultarFacturadePagoGerente : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnconsultar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void gridfactura_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ScriptManager.RegisterStartupScript(this, this.GetType(), "Pop", "$('#modal-detalle').modal('show');", true);
+        }
+
+        protected void griddetallefactura_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //este es el grid del modal del popup
+        }
+    }
+}
